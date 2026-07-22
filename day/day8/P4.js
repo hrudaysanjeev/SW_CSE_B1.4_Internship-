9662dddc648abe3bdf553835b397ad52640af624
hrudaysanjeev <sanjeevkatta2008@gmail.com>
2026-07-22 12:44:03 +0530

    added let for variable

---
+++ day/day8/P4.js
@@ -0,0 +1,19 @@
+var x=10;
+var x=20;
+x=30;
+console.log(x)
+
+if (true) {
+    var message= "hello";
+}
+console.log(message);
+
+let score= 50;
+// let score= 50;
+score=100;
+console.log(score)
+
+if(true){
+    let secret="sanju@123";
+}
+console.log(secret);
